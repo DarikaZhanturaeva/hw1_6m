40cd683f4a1e0b742ef4eb374f3cc43cd490444a
Darika <darikazhanturaeva@gmail.com>
2024-05-23 20:31:41 +0600

    Initial commit

---
+++ app/src/main/java/com/geeks/hw1_6m/Counter.kt
@@ -0,0 +1,14 @@
+package com.geeks.hw1_6m
+
+class Counter {
+
+    private var count = 0
+
+    fun inc(){
+        count++
+    }
+    fun dec(){
+        count--
+    }
+    fun getCountResult()= count
+}
---
+++ app/src/main/java/com/geeks/hw1_6m/CounterViewModel.kt
@@ -0,0 +1,19 @@
+package com.geeks.hw1_6m
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class CounterViewModel : ViewModel(){
+
+    private val counter = Counter()
+    private val counterData = MutableLiveData<Int>()
+    val counterLD : LiveData<Int>
+        get() = counterData
+
+     fun updateCounterData(isInc : Boolean){
+        if (isInc) counter.inc() else counter.dec()
+        counterData.value=counter.getCountResult()
+    }
+    //fun counterData(): LiveData<Int> = counterData
+}
